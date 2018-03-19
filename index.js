2696bfecabb977a153ebbf44bf6ad141a5acff2c
Sander Huijsen <sander.huijsen@gmail.com>
2018-03-19 14:36:18 +0800

    Changed name. Added handlers for command line arguments.

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
+const args = require('command-line-args');
 const fs = require('fs-extra');
 const path = require('path');
 const request = require('request');
 const st = require('speedtest-net')({ maxTime: 12000 });
+const usage = require('command-line-usage');
 
 const stats = {};
+
+const optionDefs = [
+  {
+    name: 'command',
+    defaultOption: true,
+  },
+  {
+    name: 'help',
+    alias: 'h',
+    type: Boolean,
+    description: 'Display usage',
+  },
+  {
+    name: 'quiet',
+    alias: 'q',
+    type: Boolean,
+    description: 'Suppress all output',
+  },
+  {
+    name: 'start',
+    description: 'Start automatic testing',
+  },
+  {
+    name: 'stop',
+    description: 'Stop automatic testing',
+  }
+];
 
 /**
  * Post speedtest results
@@ -31,22 +60,47 @@ const postResults = (token) => new Promise((resolve, reject) => {
  */
 const getToken = async () => {
   const filepath = path.resolve(process.env['HOME'], '.st', 'config');
-  return await fs.readJson(filepath);
+  try {
+    return await fs.readJson(filepath);
+  } catch (e) {
+    return null;
+  }
 };
 
 /**
- * Upload results
+ * Run the test
  */
-const uploadResults = async () => {
+const test = async () => {
   const token = await getToken();
-  if (token) postResults(token);
+  if (!token) {
+    console.log('Can\'t find token in ~/.st/config.');
+    process.exit(255);
+  }
+
+  st.on('data', data => {
+    stats.data = data;
+  });
+
+  st.on('done', () => {
+    postResults(token);
+  });
 };
 
-st.on('data', data => {
-  stats.data = data;
-});
+const options = args(optionDefs);
+console.log(JSON.stringify(options, null, 3));
+if (options.help) {
+  console.log(usage([
+    {
+      header: 'Options',
+      optionList: optionDefs,
+    }
+  ]));
+  process.exit(0);
+}
 
-st.on('done', () => {
-  uploadResults()
-    .catch(console.log);
-});
+test(options)
+  .catch(err => {
+    if (!options.quiet) {
+      console.log(err);
+    }
+  });
